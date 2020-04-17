99e63638635432d1c439bb8a5566e8171dd199be
KangJunewoo <brianjune@naver.com>
2020-04-17 15:36:37 +0900

    gitignore 작동을 위해 잠시 백업

--- project/comedu/graduate/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-#단순히 졸업요건 보여주는 것이라면 굳이 모델 필요 x 일 수도 있다.
-# Create your models here.
--- project/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\web1project\cmdweb1teamproject\project\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
