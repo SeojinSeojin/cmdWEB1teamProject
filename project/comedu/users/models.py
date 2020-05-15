7da74a5366254bf85cc5643247f0c02f0c418384
KangJunewoo <brianjune@naver.com>
2020-05-15 23:01:18 +0900

    delete migrations + user model fixme

--- project/comedu/users/models.py
+++ project/comedu/users/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     email = models.EmailField(verbose_name = "email", max_length = 255, unique = True)
+    # FIXME : studentID는 unique한데, superuser 또한 이 model에 들어가므로
+    # 두번째 superuser 만들때 studentID 중복문제 발생(공백으로)
     studentID = models.CharField(max_length=10, unique = True)
 
     avatar = models.ImageField(blank=True)
